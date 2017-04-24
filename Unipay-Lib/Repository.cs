59ccdb1deae80c93322722278a731e2fa74515ec
andr9528 <andr9528@edu.eal.dk>
2017-04-24 09:29:23 +0200

    Repository Has been set up with the ground elements needed for it to work, still needs the different lists when those have been created

--- Unipay-Lib/Repository.cs
+++ Unipay-Lib/Repository.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Unipay_Lib
 {
-    public class Repository
+    public sealed class Repository
     {
+        static readonly Repository repository = new Repository();
+        //
+        // Instantiate any lists stored in this repository
+        //
+        private Repository()
+        {
+            
+        }
+        //
+        // Return a version of the repository to whereever it is called.
+        // Remembers any data that is put in to the repository.
+        //
+        public static Repository GetRepository()
+        {
+            return repository;
+        }
+        //
+        // should take one argument, per list that is to be stored in the repository
+        //
+        public void GetLists()
+        {
+
+        }
     }
 }
