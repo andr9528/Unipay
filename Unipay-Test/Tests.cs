baf488599598b63c21153860daf6784c0be98f7d
andr9528 <andr9528@edu.eal.dk>
2017-04-24 08:53:26 +0200

    Created all (or close to all) classes, windows, and pages needed to get startet

---
+++ Unipay-Test/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unipay_Test
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
